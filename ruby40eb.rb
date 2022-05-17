5e0b122564f031b602b8d50776cc307c0a70310e
kutidake <kutidake@icloud.com>
2022-05-17 21:00:31 +0900

    22/5/17

---
+++ ruby40eb.rb
@@ -0,0 +1,18 @@
+def xyz_there(str)
+  # 処理を記述
+  if str.include?(".xyz")
+    puts "False"
+  elsif
+    str.include?("xyz")
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+# 呼び出し例
+xyz_there('abcxyz')
+xyz_there('abc.xyz')
+xyz_there('xyz.abc')
+xyz_there('azbycx')
+xyz_there('a.zbycx')
